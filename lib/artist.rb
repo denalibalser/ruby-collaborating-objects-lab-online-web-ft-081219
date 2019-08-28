a8f91a95e8dbd189e391bb1624465e20207ba167
Denali Balser <denali.balser167@gmail.com>
2019-08-28 21:09:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,12 +19,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    if self.all.find{|artist_obj| artist_obj.name == artist_name}
-      self.all.find{|artist_obj| artist_obj.name == artist_name}
-    else
-      self.new(artist_name)
-    end
-  end 
+    self.all.find{|artist_obj| artist_obj.name == artist_name} || self.new(artist_name)
+  end
 
 
   def save
